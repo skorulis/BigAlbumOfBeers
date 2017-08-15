8388f66d60be9dbfc67890ab6e1694d125873ad6
Alex Skorulis <skorulis@gmail.com>
2017-08-15 15:00:46 +1000

    Get hours from google places

--- scripts/getPlaces.rb
+++ scripts/getPlaces.rb
@@ -12,4 +12,42 @@ end
 
 
 data = JSON.parse(File.read(placeFilename))
-puts data
+array = data[data.keys[0]]
+
+array.each do |place|
+	placeId = place["place_id"]
+	if placeId == nil && place["extra"] != nil
+		placeId = place["extra"]["place_id"]
+	end
+
+	if placeId != nil
+		url = "https://maps.googleapis.com/maps/api/place/details/json?key=#{apiKey}&placeid=#{placeId}"
+		filename = "_data/places/#{placeId}.json"
+
+		puts url
+
+		clnt = HTTPClient.new;
+		data = clnt.get_content(url)
+		result = JSON.parse(data)
+		result["result"]["opening_hours"].delete("open_now")
+
+		File.open(filename,"w") do |f|
+  			f.write(JSON.pretty_generate(result))
+		end
+	end
+end
+
+files = Dir["./_data/places/**/*.json"]
+
+allHours = Hash.new
+
+files.each do |f|
+	result = JSON.parse(File.read(f))
+	hours = result["result"]["opening_hours"]
+	key = f.split('/')[-1].sub(".json","")
+	allHours[key] = hours
+end
+
+File.open("_data/allHours.json","w") do |f|
+  		f.write(JSON.pretty_generate(allHours))
+	end
